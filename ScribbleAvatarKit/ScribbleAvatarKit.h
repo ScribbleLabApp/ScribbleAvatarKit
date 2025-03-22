c51666582535bdc5a7f6f47d46bf09289f6a6ad5
N3v1 <n3v1010@gmail.com>
2025-03-22 11:47:58 +0100

    Initial Commit

---
+++ ScribbleAvatarKit/ScribbleAvatarKit.h
@@ -0,0 +1,18 @@
+//
+//  ScribbleAvatarKit.h
+//  ScribbleAvatarKit
+//
+//  Created by Nevio Hirani on 22.03.25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ScribbleAvatarKit.
+FOUNDATION_EXPORT double ScribbleAvatarKitVersionNumber;
+
+//! Project version string for ScribbleAvatarKit.
+FOUNDATION_EXPORT const unsigned char ScribbleAvatarKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ScribbleAvatarKit/PublicHeader.h>
+
+
